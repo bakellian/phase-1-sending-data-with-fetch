c203601305fd4ff6ef27a1a89784607aa90a8e5e
Bridget Akellian <akellianbv@gmail.com>
2022-02-26 23:01:38 -0500

    commit tree 2e32158773314c40f5395f2a8e2bf963e95f3b6e

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email) {
+    let dataObj = {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            "name": name,
+            "email": email
+        })
+    };
+    return fetch("http://localhost:3000/users", dataObj)
+    .then(res => res.json())
+    .then(body => addUser(body.id))
+    .catch(res => document.body.innerHTML = alert("Unauthorized Access"))
+}
+
+function addUser(newUser) {
+    document.getElementsByTagName("body")[0].innerHTML = newUser;
+    document.body.appendChild(message)
+}
+
+submitData("Lily", "lily@pad.com")
